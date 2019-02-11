144c7cf04359823907b7eb148dd5c071bd18f02a
Shannan Kammann <sbkammann@gmail.com>
2019-02-11 14:56:07 -0800

    feat: Added basic HTML

---
+++ js/app.js
@@ -0,0 +1,8 @@
+// When I enter text into the #editor element, the #preview element is updated as I type to display the content of the textarea.
+//When I enter GitHub flavored markdown into the #editor element, the text is rendered as HTML in the #preview element as I type
+// (HINT: You don't need to parse Markdown yourself - you can import the Marked library for this: https://cdnjs.com/libraries/marked).
+//When my markdown previewer first loads, the default text in the #editor field should contain valid markdown that represents at least
+// one of each of the following elements: a header (H1 size), a sub header (H2 size), a link, inline code, a code block, a list item, a blockquote, an image, and bolded text.
+//When my markdown previewer first loads, the default markdown in the #editor field should be rendered as HTML in the #preview element.
+//When I click a link rendered by my markdown previewer, the link is opened up in a new tab (HINT: read the Marked.js docs for this one!).
+//My markdown previewer interprets carriage returns and renders them as br (line break) elements.
