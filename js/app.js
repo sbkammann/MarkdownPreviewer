ea4845fd14096dd9b00fd2a4a70f5324b5d353ae
Shannan Kammann <sbkammann@gmail.com>
2019-02-11 22:05:48 -0800

    feat: Added Marked.js functionality.

--- js/app.js
+++ js/app.js
@@ -6,3 +6,22 @@
 //When my markdown previewer first loads, the default markdown in the #editor field should be rendered as HTML in the #preview element.
 //When I click a link rendered by my markdown previewer, the link is opened up in a new tab (HINT: read the Marked.js docs for this one!).
 //My markdown previewer interprets carriage returns and renders them as br (line break) elements.
+// a header (H1 size), a sub header (H2 size), a link, inline code, a code block, a list item, a blockquote, an image, and bolded text.
+
+const editor = document.getElementById('editor');
+const preview = document.getElementById('preview');
+const exampleArr = ['# Markdown Previewer', '## An FCC project','`Type Markdown in the editor window and see it displayed in the preview window.`', '[Link to markdown cheatsheet on github](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet)', ' 1. This is an example of a first ordered list item',
+          '2. This is another example', ' > This is a Blockquotes.', ' **Try it out for yourself!**', 'Inline-style Image: ![alt text](https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png "Logo Title Text 1")'];
+window.addEventListener('load', function(event){
+  exampleArr.forEach(el=> preview.innerHTML += marked(el));
+  exampleArr.forEach(el=> editor.textContent += el +'\n');
+});
+
+editor.addEventListener('input', function(event){ //need a different event
+  // if(event.key === 'Enter'){
+  //   console.log('enter')
+  //   preview.appendChild('<br>');
+  // }
+
+  preview.innerHTML = marked(`${editor.value}`);
+});
